1689e5c487586e9bff48f7d35bc5851ef0380a21
Jacob Ioffe <jioffe@nvidia.com>
2026-05-26 20:22:50 +0000

    Add SPDX header to type alias tests

--- nemo_retriever/tests/test_type_aliases.py
+++ nemo_retriever/tests/test_type_aliases.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from importlib import import_module
 from typing import get_args
 
